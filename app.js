694afb1ef7b242e831de6ac5a1461dbec453ec25
Kisekios <dekostump@gmail.com>
2024-04-19 23:02:08 -0500

    Diseño header fase 1

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+
+const PORT = process.env.PORT ?? 1234
+app.disable('x-powered-by')
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
+
+app.listen(PORT, () => {
+  console.log(`server listening on port http://localhost:${PORT}`)
+})
