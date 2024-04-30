66d2716589bc2bd9eec8a8b36b3d0ee597377f85
Kisekios <dekostump@gmail.com>
2024-04-29 21:54:15 -0500

    layaout pagina destino-url ok

--- public/destinos.js
+++ public/destinos.js
@@ -1,6 +1,6 @@
 const obtenerUrl = (globalThis.location.pathname).split('/')
 
-fetch('http://localhost:1234/destinos.json')
+fetch('http://192.168.0.8:1234//destinos.json')
   .then(res => res.json())
   .then(response => {
     const destino = response.find(x => x.id === obtenerUrl[2])
