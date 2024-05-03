b3f6b19c6808e4aec95f554208b1a13e0a01dd55
Kisekios <dekostump@gmail.com>
2024-05-02 21:46:17 -0500

    Cambio del DOM por JS en pagina destino

--- public/destinos.js
+++ public/destinos.js
@@ -6,5 +6,76 @@ fetch('http://192.168.0.8:1234//destinos.json')
     const destino = response.find(x => x.id === obtenerUrl[2])
 
     const imagen = document.querySelector('.imagen')
+    const nameDestino = document.querySelector('.destino-name')
+    const descripcionDestino = document.querySelector('.destino-descripcion')
+    const incluyeDestino = document.querySelector('.destino-incluye')
+
     imagen.setAttribute('src', destino.banner)
+    nameDestino.innerHTML = destino.titulo
+    descripcionDestino.innerHTML = destino.descripcion
+    incluyeDestino.innerHTML = destino.incluye
+
+    descripcionHoteles(destino.hoteles)
+    actividadesDestino(destino.actividades)
+    informacionAdicionalDestino(destino.informacion)
   })
+
+function descripcionHoteles (hoteles) {
+  const hotelesDestino = document.querySelector('.destino-hoteles')
+
+  for (hotel of hoteles) {
+    const hotelCard = document.createElement('div')
+
+    const hotelName = document.createElement('h4')
+    hotelName.innerHTML = hotel.nombreHotel
+
+    const hotelDescripcion = document.createElement('h5')
+    hotelDescripcion.innerHTML = 'Cuenta con:'
+
+    const hotelOfrece = document.createElement('ul')
+
+    hotel.ofrece.forEach(element => {
+      const puntoOfreceHotel = document.createElement('li')
+      puntoOfreceHotel.innerHTML = '→ ' + element
+
+      hotelOfrece.appendChild(puntoOfreceHotel)
+    })
+
+    hotelCard.appendChild(hotelName)
+    hotelCard.appendChild(hotelDescripcion)
+    hotelCard.appendChild(hotelOfrece)
+    hotelesDestino.appendChild(hotelCard)
+  }
+}
+
+function actividadesDestino (actividades) {
+  const actividadesDestino = document.querySelector('.destino-actividades')
+
+  for (opcion of actividades) {
+    const actividadCard = document.createElement('div')
+
+    const actividadName = document.createElement('h3')
+    actividadName.innerHTML = opcion.actividad
+
+    const actividadImagen = document.createElement('img')
+    actividadImagen.setAttribute('src', opcion.imagen)
+
+    actividadCard.appendChild(actividadName)
+    actividadCard.appendChild(actividadImagen)
+
+    actividadesDestino.appendChild(actividadCard)
+  }
+}
+
+function informacionAdicionalDestino (informacion) {
+  const informacionDestino = document.querySelector('.informacion-destino')
+  const informacionAdicional = document.createElement('ul')
+
+  for (nota of informacion) {
+    const notaAdicional = document.createElement('li')
+    notaAdicional.innerHTML = nota
+
+    informacionAdicional.appendChild(notaAdicional)
+  }
+  informacionDestino.appendChild(informacionAdicional)
+}
