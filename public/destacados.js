5f37fc1f90a115305ce04c744bfbd6928f18920f
Kisekios <dekostump@gmail.com>
2024-05-08 00:16:34 -0500

    lectura del json para hacer los banner de destacados en pagina inicial y destinos

--- public/destacados.js
+++ public/destacados.js
@@ -1,13 +1,38 @@
-fetch('http://192.168.0.8:1234/destacados.json')
+const obtenerUrl = (globalThis.location.pathname).split('/')
+
+// Inyeccion de imagen y url de planes destacados/promociones
+fetch('http://192.168.0.16:1234/destinos.json')
   .then(res => res.json())
   .then(response => {
     const destacados = document.querySelector('.destacados')
-    for (destacado of response) {
+    if (obtenerUrl[1] === '') { // inyección al index
+      buscarIndiceParaOrdenar(0, 2)
+    } else if (obtenerUrl[1] === 'nacionales') {
+      buscarIndiceParaOrdenar(1, 3)
+    } else if (obtenerUrl[1] === 'internacionales') {
+      buscarIndiceParaOrdenar(1, 4)
+    }
+
+    function buscarIndiceParaOrdenar(n1, n2) {
+      const destacados = []
+      for (let i = 0; i < response.length; i++) {
+        if (response[i].destacado[n1] && response[i].destacado[n2] !== 0) {
+          destacados.push(response[i])
+        }
+      }
+
+      for (let i = 1; i < destacados.length + 1; i++) {
+        const orden = destacados.find(x => x.destacado[n2] === i)
+        renderDestacados(orden.banner, orden.url)
+      }
+    }
+
+    function renderDestacados(imagen, url) {
       const enlaceDestacado = document.createElement('a')
-      enlaceDestacado.setAttribute('href', destacado.url)
+      enlaceDestacado.setAttribute('href', url)
 
       const imageDestacado = document.createElement('img')
-      imageDestacado.setAttribute('src', destacado.imagen)
+      imageDestacado.setAttribute('src', imagen)
 
       enlaceDestacado.appendChild(imageDestacado)
       destacados.appendChild(enlaceDestacado)
