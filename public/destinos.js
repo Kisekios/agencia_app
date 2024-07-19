300126843c5cfb01c6ce5a31834160459473dce8
Kisekios <dekostump@gmail.com>
2024-07-18 23:36:47 -0500

    agregado desplazamiento al home y a la pagina de destinos

--- public/destinos.js
+++ public/destinos.js
@@ -61,7 +61,7 @@ function descripcionHoteles(hoteles) {
     divContenedorCards.appendChild(hotelCard)
     hotelesDestino.appendChild(divContenedorCards)
   }
-  globalThis.onload = desplazamientoDivHoteles(hotelesDestino)
+  //globalThis.onload = desplazamientoDivHoteles(hotelesDestino)
 }
 
 function actividadesDestino(actividades) {
@@ -94,105 +94,4 @@ function informacionAdicionalDestino(informacion) {
     informacionAdicional.appendChild(notaAdicional)
   }
   informacionDestino.appendChild(informacionAdicional)
-}
-
-function desplazamientoDivHoteles(contenedor) {
-  let desplazamiento
-  let ajusteSegunPantalla
-  let posicion = 1
-  let scrollReverseX = false
-  let banderaX = false
-  let banderaY = false
-  let banderaIntervalo = false
-  let intervalo
-  let pausarDesplazamiento
-  let aumentarTiempoPausa
-
-  function ajustarDesplazamiento() {
-    let anchoPagina = globalThis.innerWidth;
-    if (anchoPagina <= 500) {
-      desplazamiento = 2;
-    } else {
-      desplazamiento = 1;
-    }
-  }
-
-  function iniciarIntervalo() {
-    if (!banderaX && !banderaY && !banderaIntervalo) {
-      banderaIntervalo = true
-      clearInterval(intervalo)
-      contenedor.style.scrollSnapType = "none"
-      setTimeout(() => {
-        intervalo = setInterval(function () {
-          let anchoDiv = contenedor.scrollWidth - contenedor.offsetWidth
-          if (posicion <= anchoDiv && !scrollReverseX) {
-            contenedor.scroll(posicion, 0)
-            posicion++
-          } else {
-
-            setTimeout(() => {
-              scrollReverseX = true
-            }, 1000)
-            posicion--
-            contenedor.scroll(posicion, 0)
-            if (posicion <= 1) {
-              setTimeout(() => {
-                scrollReverseX = false
-              }, 1000)
-            }
-          }
-        }, 30 / desplazamiento);
-      }, 1000)
-    }
-  }
-
-  function detenerIntervalo() {
-    if (!banderaX && !banderaY) {
-      banderaX = true
-      clearInterval(intervalo)
-      contenedor.style.scrollSnapType = "x mandatory"
-      pausarDesplazamiento = setTimeout(() => {
-        banderaX = false
-        reanudarDesplazamiento()
-      }, 5000)
-    } else {
-      if (banderaX) {
-        banderaY = true
-        clearTimeout(pausarDesplazamiento)
-        clearTimeout(aumentarTiempoPausa)
-        aumentarTiempoPausa = setTimeout(() => {
-          banderaX = false
-          banderaY = false
-          reanudarDesplazamiento()
-        }, 5000)
-      }
-    }
-  }
-
-  function reanudarDesplazamiento() {
-    clearInterval(intervalo)
-    if (!banderaX && !banderaY && banderaIntervalo) {
-      banderaIntervalo = false
-      posicion = contenedor.scrollLeft
-      ajustarDesplazamiento();
-      iniciarIntervalo();
-    }
-  }
-
-  contenedor.addEventListener('touchstart', detenerIntervalo);
-  contenedor.addEventListener('click', detenerIntervalo);
-
-  contenedor.addEventListener('scroll', () => {
-    if (Math.abs(contenedor.scrollLeft - posicion) > 5) {
-      detenerIntervalo()
-    }
-  })
-
-  ajustarDesplazamiento();
-  iniciarIntervalo();
-
-  window.addEventListener('resize', () => {
-    clearInterval(intervalo)
-    detenerIntervalo()
-  });
 }
--- public/main.js
+++ public/main.js
@@ -7,9 +7,11 @@ const menuOpcionesMovil = document.querySelector('.menu-opciones-movil')
 
 /* Variables */
 
+const urlPath = (globalThis.location.pathname).split('/')
+
 /* Funciones */
 
-function mostrarOpcionesMovil () {
+function mostrarOpcionesMovil() {
   menuOpcionesMovil.classList.toggle('inactive')
   menuIconMovil.classList.toggle('cambio')
 }
@@ -36,12 +38,12 @@ fetch(globalThis.origin + '/informacion')
     const termYCondiciones = document.createElement('h6')
 
     contactenos.innerHTML = informacion.contacto
-    
+
     const linkRedes = document.createElement('div')
 
     renderLinkRedes(linkRedes, informacion.links)
     const lista = document.createElement('ul')
-    renderItemsList(informacion.ubicacion,informacion.telefono, lista)
+    renderItemsList(informacion.ubicacion, informacion.telefono, lista)
 
     redesYUbicacion.appendChild(linkRedes)
     redesYUbicacion.appendChild(lista)
@@ -56,7 +58,7 @@ fetch(globalThis.origin + '/informacion')
 
     const imagenAsesor = document.createElement('img')
     imagenAsesor.setAttribute('src', informacion.foto)
-    
+
     informacionAsesor.appendChild(seccionAsesor)
     informacionAsesor.appendChild(imagenAsesor)
 
@@ -73,47 +75,168 @@ fetch(globalThis.origin + '/informacion')
     footer.appendChild(section)
   })
 
-  function renderLinkRedes (div,links) {
-    for (const link of links) {
-      const anchor = document.createElement('a')
-      anchor.setAttribute('href', link.link)
-      anchor.classList.add('fa')
-      anchor.classList.add(link.simbolo)
-      div.appendChild(anchor)
+function renderLinkRedes(div, links) {
+  for (const link of links) {
+    const anchor = document.createElement('a')
+    anchor.setAttribute('href', link.link)
+    anchor.classList.add('fa')
+    anchor.classList.add(link.simbolo)
+    div.appendChild(anchor)
+  }
+}
+
+function renderItemsList(ubicacion, telefono, lista) {
+  const ubicacionYDireccion = document.createElement('li')
+
+  const simboloUbicacion = document.createElement('p')
+  simboloUbicacion.classList.add('fa')
+  simboloUbicacion.classList.add(ubicacion.simbolo)
+  ubicacionYDireccion.appendChild(simboloUbicacion)
+
+  const sede = document.createElement('p')
+  sede.innerHTML = ubicacion.sede
+  ubicacionYDireccion.appendChild(sede)
+
+  const lugar = document.createElement('p')
+  lugar.innerHTML = ubicacion.edificio
+  ubicacionYDireccion.appendChild(lugar)
+
+  const address = document.createElement('p')
+  address.innerHTML = ubicacion.direccion
+  ubicacionYDireccion.appendChild(address)
+
+  lista.appendChild(ubicacionYDireccion)
+
+  const numeroDeTelefono = document.createElement('li')
+  const simboloTelefono = document.createElement('p')
+  simboloTelefono.classList.add('fa')
+  simboloTelefono.classList.add(telefono.simbolo)
+  numeroDeTelefono.appendChild(simboloTelefono)
+
+  const numeroTelefono = document.createElement('p')
+  numeroTelefono.innerHTML = telefono.numero
+  numeroDeTelefono.appendChild(numeroTelefono)
+
+  lista.appendChild(numeroDeTelefono)
+}
+
+function iniciarDesplazamientoDiv(ubicacion) {
+  if (ubicacion === 'destino' || ubicacion === 'plan-turistico') {
+    const hotelesODias = document.querySelector('.destino-hoteles')
+    desplazamientoDiv(hotelesODias)
+  } else if (ubicacion === 'nacionales' || ubicacion === 'internacionales') {
+    const nacionalesOInternacionales = document.querySelector('.destacados')
+    desplazamientoDiv(nacionalesOInternacionales)
+  } else if (ubicacion === '') {
+    const homeDestacados = document.querySelector('.destacados')
+    desplazamientoDiv(homeDestacados)
+  } else {
+    console.log('sitio no configurado')
+  }
+}
+
+function desplazamientoDiv(contenedor) {
+  console.log('iniciando proceso ')
+  let desplazamiento
+  let posicion = 1
+  let scrollReverseX = false
+  let banderaX = false
+  let banderaY = false
+  let banderaIntervalo = false
+  let intervalo
+  let pausarDesplazamiento
+  let aumentarTiempoPausa
+
+  function ajustarDesplazamiento() {
+    let anchoPagina = globalThis.innerWidth;
+    if (anchoPagina <= 500) {
+      desplazamiento = 2;
+    } else {
+      desplazamiento = 1;
     }
   }
 
-  function renderItemsList(ubicacion, telefono, lista) {
-    const ubicacionYDireccion = document.createElement('li')
+  function iniciarIntervalo() {
+    if (!banderaX && !banderaY && !banderaIntervalo) {
+      banderaIntervalo = true
+      contenedor.style.scrollSnapType = "none"
+      clearInterval(intervalo)
+      setTimeout(() => {
+        intervalo = setInterval(function () {
+          let anchoDiv = contenedor.scrollWidth - contenedor.offsetWidth
+          if (posicion <= anchoDiv && !scrollReverseX) {
+            contenedor.scroll(posicion, 0)
+            posicion++
+          } else {
 
-    const simboloUbicacion = document.createElement('p')
-    simboloUbicacion.classList.add('fa')
-    simboloUbicacion.classList.add(ubicacion.simbolo)
-    ubicacionYDireccion.appendChild(simboloUbicacion)
-
-    const sede = document.createElement('p')
-    sede.innerHTML= ubicacion.sede
-    ubicacionYDireccion.appendChild(sede)
-
-    const lugar = document.createElement('p')
-    lugar.innerHTML = ubicacion.edificio
-    ubicacionYDireccion.appendChild(lugar)
-
-    const address = document.createElement('p')
-    address.innerHTML = ubicacion.direccion
-    ubicacionYDireccion.appendChild(address)
-
-    lista.appendChild(ubicacionYDireccion)
-
-    const numeroDeTelefono = document.createElement('li')
-    const simboloTelefono = document.createElement('p')
-    simboloTelefono.classList.add('fa')
-    simboloTelefono.classList.add(telefono.simbolo)
-    numeroDeTelefono.appendChild(simboloTelefono)
-
-    const numeroTelefono = document.createElement('p')
-    numeroTelefono.innerHTML = telefono.numero
-    numeroDeTelefono.appendChild(numeroTelefono)
-
-    lista.appendChild(numeroDeTelefono)
+            setTimeout(() => {
+              scrollReverseX = true
+            }, 1000)
+            posicion--
+            contenedor.scroll(posicion, 0)
+            if (posicion <= 1) {
+              setTimeout(() => {
+                scrollReverseX = false
+              }, 1000)
+            }
+          }
+        }, 30 / desplazamiento);
+      }, 1000)
+    }
   }
+
+  function detenerIntervalo() {
+    console.log('detenido')
+    if (!banderaX && !banderaY) {
+
+      banderaX = true
+      clearInterval(intervalo)
+      contenedor.style.scrollSnapType = "x mandatory"
+      pausarDesplazamiento = setTimeout(() => {
+        banderaX = false
+        reanudarDesplazamiento()
+      }, 5000)
+    } else {
+      if (banderaX) {
+        banderaY = true
+        clearTimeout(pausarDesplazamiento)
+        clearTimeout(aumentarTiempoPausa)
+        aumentarTiempoPausa = setTimeout(() => {
+          banderaX = false
+          banderaY = false
+          reanudarDesplazamiento()
+        }, 5000)
+      }
+    }
+  }
+
+  function reanudarDesplazamiento() {
+    clearInterval(intervalo)
+    if (!banderaX && !banderaY && banderaIntervalo) {
+      banderaIntervalo = false
+      posicion = contenedor.scrollLeft
+      ajustarDesplazamiento();
+      iniciarIntervalo();
+    }
+  }
+
+  contenedor.addEventListener('touchstart', detenerIntervalo);
+  contenedor.addEventListener('click', detenerIntervalo);
+
+  globalThis.onload = contenedor.addEventListener('scroll', () => {
+    if (Math.abs(contenedor.scrollLeft - posicion) > 5) {
+      detenerIntervalo()
+    }
+  })
+
+  ajustarDesplazamiento();
+  iniciarIntervalo();
+
+  globalThis.onload = window.addEventListener('resize', () => {
+    clearInterval(intervalo)
+    detenerIntervalo()
+  });
+}
+
+globalThis.onload = iniciarDesplazamientoDiv(urlPath[1])
+
